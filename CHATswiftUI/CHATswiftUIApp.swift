87056a949ae5c14bf8127cbf9dba648aa3cff004
Ed <Siniakov>
2021-10-26 16:44:53 +0300

    Initial Commit

---
+++ CHATswiftUI/CHATswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHATswiftUIApp.swift
+//  CHATswiftUI
+//
+//  Created by Eduard Sinyakov on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct CHATswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
